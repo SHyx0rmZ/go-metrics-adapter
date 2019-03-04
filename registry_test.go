274b54f8dfbfe2c1d46b000d8390162a3902c02f
Patrick Pokatilo <mail@shyxormz.net>
2019-03-04 19:47:29 +0100

    Get rid of aliases in registry_test.go

--- registry_test.go
+++ registry_test.go
@@ -6,27 +6,9 @@ import (
 	"testing"
 )
 
-func NewCounter() metrics.Counter {
-	return metrics.NewCounter()
-}
-
-func NewGauge() metrics.Gauge {
-	return metrics.NewGauge()
-}
-
-func NewMeter() metrics.Meter {
-	return metrics.NewMeter()
-}
-
-func NewTimer() metrics.Timer {
-	return metrics.NewTimer()
-}
-
-type Counter metrics.Counter
-
 func BenchmarkRegistry(b *testing.B) {
 	r := NewRegistry(prometheus.NewRegistry())
-	r.Register("foo", NewCounter())
+	r.Register("foo", metrics.NewCounter())
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		r.Each(func(string, interface{}) {})
@@ -38,21 +20,21 @@ func BenchmarkRegistryParallel(b *testing.B) {
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			r.GetOrRegister("foo", NewCounter())
+			r.GetOrRegister("foo", metrics.NewCounter())
 		}
 	})
 }
 
 func TestRegistry(t *testing.T) {
 	r := NewRegistry(prometheus.NewRegistry())
-	r.Register("foo", NewCounter())
+	r.Register("foo", metrics.NewCounter())
 	i := 0
 	r.Each(func(name string, iface interface{}) {
 		i++
 		if "foo" != name {
 			t.Fatal(name)
 		}
-		if _, ok := iface.(Counter); !ok {
+		if _, ok := iface.(metrics.Counter); !ok {
 			t.Fatal(iface)
 		}
 	})
@@ -69,16 +51,16 @@ func TestRegistry(t *testing.T) {
 
 func TestRegistryDuplicate(t *testing.T) {
 	r := NewRegistry(prometheus.NewRegistry())
-	if err := r.Register("foo", NewCounter()); nil != err {
+	if err := r.Register("foo", metrics.NewCounter()); nil != err {
 		t.Fatal(err)
 	}
-	if err := r.Register("foo", NewGauge()); nil == err {
+	if err := r.Register("foo", metrics.NewGauge()); nil == err {
 		t.Fatal(err)
 	}
 	i := 0
 	r.Each(func(name string, iface interface{}) {
 		i++
-		if _, ok := iface.(Counter); !ok {
+		if _, ok := iface.(metrics.Counter); !ok {
 			t.Fatal(iface)
 		}
 	})
@@ -89,12 +71,12 @@ func TestRegistryDuplicate(t *testing.T) {
 
 func TestRegistryGet(t *testing.T) {
 	r := NewRegistry(prometheus.NewRegistry())
-	r.Register("foo", NewCounter())
-	if count := r.Get("foo").(Counter).Count(); 0 != count {
+	r.Register("foo", metrics.NewCounter())
+	if count := r.Get("foo").(metrics.Counter).Count(); 0 != count {
 		t.Fatal(count)
 	}
-	r.Get("foo").(Counter).Inc(1)
-	if count := r.Get("foo").(Counter).Count(); 1 != count {
+	r.Get("foo").(metrics.Counter).Inc(1)
+	if count := r.Get("foo").(metrics.Counter).Count(); 1 != count {
 		t.Fatal(count)
 	}
 }
@@ -103,9 +85,9 @@ func TestRegistryGetOrRegister(t *testing.T) {
 	r := NewRegistry(prometheus.NewRegistry())
 
 	// First metric wins with GetOrRegister
-	_ = r.GetOrRegister("foo", NewCounter())
-	m := r.GetOrRegister("foo", NewGauge())
-	if _, ok := m.(Counter); !ok {
+	_ = r.GetOrRegister("foo", metrics.NewCounter())
+	m := r.GetOrRegister("foo", metrics.NewGauge())
+	if _, ok := m.(metrics.Counter); !ok {
 		t.Fatal(m)
 	}
 
@@ -115,7 +97,7 @@ func TestRegistryGetOrRegister(t *testing.T) {
 		if name != "foo" {
 			t.Fatal(name)
 		}
-		if _, ok := iface.(Counter); !ok {
+		if _, ok := iface.(metrics.Counter); !ok {
 			t.Fatal(iface)
 		}
 	})
@@ -128,9 +110,9 @@ func TestRegistryGetOrRegisterWithLazyInstantiation(t *testing.T) {
 	r := NewRegistry(prometheus.NewRegistry())
 
 	// First metric wins with GetOrRegister
-	_ = r.GetOrRegister("foo", NewCounter)
-	m := r.GetOrRegister("foo", NewGauge)
-	if _, ok := m.(Counter); !ok {
+	_ = r.GetOrRegister("foo", metrics.NewCounter)
+	m := r.GetOrRegister("foo", metrics.NewGauge)
+	if _, ok := m.(metrics.Counter); !ok {
 		t.Fatal(m)
 	}
 
@@ -140,7 +122,7 @@ func TestRegistryGetOrRegisterWithLazyInstantiation(t *testing.T) {
 		if name != "foo" {
 			t.Fatal(name)
 		}
-		if _, ok := iface.(Counter); !ok {
+		if _, ok := iface.(metrics.Counter); !ok {
 			t.Fatal(iface)
 		}
 	})
@@ -152,9 +134,9 @@ func TestRegistryGetOrRegisterWithLazyInstantiation(t *testing.T) {
 func TestRegistryUnregister(t *testing.T) {
 	l := len(arbiter.meters)
 	r := NewRegistry(prometheus.NewRegistry())
-	r.Register("foo", NewCounter())
-	r.Register("bar", NewMeter())
-	r.Register("baz", NewTimer())
+	r.Register("foo", metrics.NewCounter())
+	r.Register("bar", metrics.NewMeter())
+	r.Register("baz", metrics.NewTimer())
 	if len(arbiter.meters) != l+2 {
 		t.Errorf("arbiter.meters: %d != %d\n", l+2, len(arbiter.meters))
 	}
